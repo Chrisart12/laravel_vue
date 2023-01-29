6c1ce168a15f55f216d483d7a8ce734d8f0ad8fa
Issifou Issa <chrisissifou@gmail.com>
2023-01-29 11:16:35 +0100

    Initialisation du projet

---
+++ resources/js/components/Comment.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <form action="" @submit.prevent="submitComment">
+      <div class="row form-group col-4 ml-auto mr-auto">
+        <label for="exampleInputPassword1">Url</label>
+        <input type="text" class="form-control" v-model="url" id="" />
+      </div>
+      <div class="row form-group col-4 ml-auto mr-auto">
+        <label for="exampleInputPassword1">Name</label>
+        <input type="text" class="form-control" v-model="name" id="" />
+      </div>
+      <div class="row form-group col-4 ml-auto mr-auto">
+        <label for="exampleInputPassword1">Body</label>
+        <input type="text" class="form-control" v-model="body" id="" />
+      </div>
+      <button type="submit" class="btn btn-primary form-group col-4 ml-auto mr-auto">Enregistrer</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            url: "",
+            name: "",
+            body: ""
+        }
+    },
+
+    methods: {
+        submitComment() {
+            
+            console.log("eeeee")
+        }
+    }
+};
+</script>
+
+<style>
+</style>
